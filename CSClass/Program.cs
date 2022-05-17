599afc5a12d50a13dbc7a76e9ccafeefa99ba2c0
m04j00 <m04j00@gmail.com>
2022-05-17 11:55:36 +0900

    #1 사용자 정의 타입 Car 클래스 제작

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -10,6 +10,33 @@ namespace CSClass
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.SetInTime();
+            car.SetOutTime();
+        }
+    }
+    class Car
+    {
+        /***
+         * carNumber : 자동차 번호
+         ***/
+        int carNumber;
+        DateTime inTime;
+        DateTime outTime;
+        /// <summary>
+        /// 자동차가 들어온 시간 설정
+        /// </summary>
+        public void SetInTime()
+        {
+            this.inTime = DateTime.Now;
+        }
+
+        /// <summary>
+        /// 자동차가 나간 시간 설정
+        /// </summary>
+        public void SetOutTime()
+        {
+            this.outTime = DateTime.Now;
         }
     }
 }
