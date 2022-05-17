c55305e6607baa71805b561e4a91ff589a08931d
m04j00 <m04j00@gmail.com>
2022-05-17 12:10:27 +0900

    #2 Random 클래스의 Nex()와 NextDouble 메소드 사용

--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -13,6 +13,18 @@ namespace CSClass
             Car car = new Car();
             car.SetInTime();
             car.SetOutTime();
+            Random random = new Random();
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            Console.WriteLine(random.Next(10, 100));
+            
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
+            Console.WriteLine(random.NextDouble());
         }
     }
     class Car
