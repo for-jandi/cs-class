25f6d103a796a8c863cfddeb45e5d625a0edddde
m04j00 <m04j00@gmail.com>
2022-06-07 12:18:49 +0900

    22-6 생성자 #12

--- CSClass/Product.cs
+++ CSClass/Product.cs
@@ -2,12 +2,21 @@
 {
     internal class Product
     {
+        internal static int counter = 0;
         public string name = "No name";
         public int price = 0;
+        private int id = -1;
+
+        public Product(string name, int price)
+        {
+            this.id = counter++;
+            this.name = name;
+            this.price = price;
+        }
 
         public override string ToString()
         {
-            return this.name + " : " + this.price;
+            return "[" + this.id + "] " + this.name + " : " + this.price;
         }
     }
 }
--- CSClass/Program.cs
+++ CSClass/Program.cs
@@ -139,6 +139,12 @@ namespace CSClass
             Console.WriteLine(MyMath.Abs(52.3));
             Console.WriteLine(MyMath.Abs(52f));
             Console.WriteLine(MyMath.Abs(52L));
+
+            Product productX = new Product("감자", 2000);
+            Product productY = new Product("고구마", 3000);
+            Console.WriteLine(productX);
+            Console.WriteLine(productY);
+            Console.WriteLine(Product.counter + "개가 생성되었습니다.");
         }
     }
     class Car
